966c4579d1081dc51841461c6500cabfc55ecfc9
Filfeni <47361476+Filfeni@users.noreply.github.com>
2021-02-20 22:31:21 -0400

    new commit

---
+++ Client/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    class Program
+    {
+        
+        static void StartClient()
+        {
+            byte[] bytes = new byte[1024];
+
+            try
+            {
+                IPAddress iPAddress = IPAddress.Loopback;
+                IPEndPoint endPoint = new IPEndPoint(iPAddress, 8080);
+                Socket client = new Socket(iPAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                client.Connect(endPoint);
+                Console.WriteLine("Connected to {0}", iPAddress);
+                while (true)
+                {
+                    string msg = Console.ReadLine();
+                    byte[] byteMsg = Encoding.ASCII.GetBytes($"{msg}<EOF>");
+                    int bytesCount = client.Send(byteMsg);
+                    int receivedCount = client.Receive(bytes);
+                    Console.WriteLine("Text received = {0}", Encoding.ASCII.GetString(bytes, 0, receivedCount-5));
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            StartClient();
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+    class Program
+    {
+        public static string data = null;
+
+        public static void StartListening()
+        {
+            byte[] bytes = new byte[1024];
+
+            IPAddress ipAddress = IPAddress.Loopback;
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 8080);
+            Socket listener = new Socket(ipAddress.AddressFamily,
+                SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                listener.Bind(localEndPoint);
+                listener.Listen(10);
+                Console.WriteLine("Waiting for a connection...");
+                Socket handler = listener.Accept();
+
+                while (true)
+                {
+                    
+                    data = null;
+
+                    while (true)
+                    {
+                        int bytesRec = handler.Receive(bytes);
+                        data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                        int eofIndex = data.IndexOf("<EOF>");
+                        if ( eofIndex > -1)
+                        {
+                            Console.WriteLine("Text received : {0}", data.Remove(eofIndex));
+                            break;
+                        }
+                    }
+
+
+                    byte[] msg = Encoding.ASCII.GetBytes(data);
+
+                    handler.Send(msg);
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            Console.WriteLine("\nPress ENTER to continue...");
+            Console.Read();
+
+        }
+
+        public static void Main(String[] args)
+        {
+            StartListening();
+        }
+    }
+}
